31a06b2678b790cee312398d2320773a793838e3
naz <naz@o2.pl>
2021-12-18 21:06:57 +0100

    Add styling

--- assets/js/active-sort-btn.js
+++ assets/js/active-sort-btn.js
@@ -7,7 +7,11 @@ sortBtn.forEach((item) => {
 			if (document.querySelector('.sort__btn--active')) {
 			document.querySelector('.sort__btn--active').classList.remove('sort__btn--active');
 			}
+			if (document.querySelector('.sort__btn--up-down')) {
+			document.querySelector('.sort__btn--up-down').classList.remove('sort__btn--up-down');
+			}
 			item.classList.add('sort__btn--active');
+			item.classList.add('sort__btn--up-down');
 		},
 		false
 	);
