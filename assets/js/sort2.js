f85fe2f0ad3e2b07a4c4dabd7c5f11a5d9510f1f
naz <naz@o2.pl>
2021-12-18 19:13:34 +0100

    Add alternative sorting script

---
+++ assets/js/sort2.js
@@ -0,0 +1,36 @@
+function sortTable() {
+	const getCellValue = (tr, idx) =>
+		tr.children[idx].innerText || tr.children[idx].textContent;
+
+	const comparer = (idx, asc) => (a, b) =>
+		((v1, v2) =>
+			v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2)
+				? v1 - v2
+				: v1.localeCompare(v2))(
+			getCellValue(asc ? a : b, idx),
+			getCellValue(asc ? b : a, idx)
+		);
+
+	document.querySelectorAll('.sort__btn').forEach((sort__btn) =>
+		sort__btn.addEventListener('click', () => {
+			sort__btn.classList.add('sort__btn--down');
+
+			document.querySelectorAll('th').forEach((th) => {
+				if (sort__btn.innerText.toLowerCase() === th.innerText.toLowerCase()) {
+					const table = document.getElementById('data');
+
+					Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
+						.sort(
+							comparer(
+								Array.from(th.parentNode.children).indexOf(th),
+								(this.asc = !this.asc)
+							)
+						)
+						.forEach((tr) => table.appendChild(tr));
+				}
+			});
+		})
+	);
+}
+
+sortTable();
