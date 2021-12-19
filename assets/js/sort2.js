44d1d00b66480776d2938508a575a66cd7a9d310
naz <naz@o2.pl>
2021-12-19 05:12:25 +0100

    Change sorting script refactoring

--- assets/js/sort2.js
+++ assets/js/sort2.js
@@ -1,35 +1,30 @@
-function sortTable() {
-	const getCellValue = (tr, idx) =>
-		tr.children[idx].innerText || tr.children[idx].textContent;
-
-	const comparer = (idx, asc) => (a, b) =>
-		((v1, v2) =>
-			v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2)
-				? v1 - v2
-				: v1.localeCompare(v2))(
-			getCellValue(asc ? a : b, idx),
-			getCellValue(asc ? b : a, idx)
+const getCellValue = (tr, idx) =>
+		tr.children[idx].innerText || tr.children[idx].textContent,
+	comparer = (idx, asc) => (a, b) => {
+		return (
+			(v1 = getCellValue(asc ? a : b, idx)),
+			(v2 = getCellValue(asc ? b : a, idx)),
+			'' === v1 || '' === v2 || isNaN(v1) || isNaN(v2)
+				? v1.localeCompare(v2)
+				: v1 - v2
 		);
+	};
 
-	document.querySelectorAll('.sort__btn').forEach((sort__btn) =>
-		sort__btn.addEventListener('click', () => {
+document.querySelectorAll('.sort__btn').forEach((sort__btn) =>
+	sort__btn.addEventListener('click', () => {
+		document.querySelectorAll('th').forEach((th) => {
+			if (sort__btn.innerText.toLowerCase() === th.innerText.toLowerCase()) {
+				const table = document.getElementById('data');
 
-			document.querySelectorAll('th').forEach((th) => {
-				if (sort__btn.innerText.toLowerCase() === th.innerText.toLowerCase()) {
-					const table = document.getElementById('data');
-
-					Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
-						.sort(
-							comparer(
-								Array.from(th.parentNode.children).indexOf(th),
-								(this.asc = !this.asc)
-							)
+				Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
+					.sort(
+						comparer(
+							Array.from(th.parentNode.children).indexOf(th),
+							(this.asc = !this.asc)
 						)
-						.forEach((tr) => table.appendChild(tr));
-				}
-			});
-		})
-	);
-}
-
-sortTable();
+					)
+					.forEach((tr) => table.appendChild(tr));
+			}
+		});
+	})
+);
